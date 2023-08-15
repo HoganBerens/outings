b7e3d7cbe3a26e4944ca8603969d7a913e5ac2d4
Chris Kildunne <136514462+kilzridesbikes@users.noreply.github.com>
2023-08-15 13:41:37 -0600

    dashboard deleted home page fixed

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("", views.dashboard, name="dashboard"),
-    path("home/", views.home, name="home"),
+    #path("", views.dashboard, name="dashboard"),
+    path("", views.home, name="home"),
     path("accounts/signup/", views.signup, name="signup"),
     path("events/", views.events_index, name="index"),
     path("events/create/", views.EventCreate.as_view(), name="events_create"),
